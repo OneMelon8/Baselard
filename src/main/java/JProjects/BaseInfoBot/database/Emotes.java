64e73c5d4ed71f616a4c1bd1d0e68bf54fa36c8e
OneMelon8 <48169582+OneMelon8@users.noreply.github.com>
2019-03-03 22:44:19 -0800

    Updated rodybeat emote

--- src/main/java/JProjects/BaseInfoBot/database/Emotes.java
+++ src/main/java/JProjects/BaseInfoBot/database/Emotes.java
@@ -1,7 +1,5 @@
 package JProjects.BaseInfoBot.database;
 
 public class Emotes {
-	public static final String rodyBeat = "<:rodybeat:549330719365791745>";
-	public static final String yeezus = "<:yeezus:549831313889427477>";
-	public static final String mini = "<:mini:549831313646157835>";
+	public static final String rodyBeat = "<:rodybeat:552017882905575424>";
 }
