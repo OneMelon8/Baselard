a12b364f3e557be41c59e6768d2ba403f80d2b78
One <One@One>
2019-08-27 15:17:17 +0800

    Update v3.3.2.3.1.12 -- Fixed RAS's logo

--- src/main/java/JProjects/BaseInfoBot/App.java
+++ src/main/java/JProjects/BaseInfoBot/App.java
@@ -14,7 +14,7 @@ public class App {
 	public static void main(String[] args) {
 		try {
 			System.out.println("Hello, Happy World!");
-			bot = new BaseInfoBot("3.3.2.3.1.11");
+			bot = new BaseInfoBot("3.3.2.3.1.12");
 			bot.addListener(new ChatEventListener());
 
 			// initGoogle();
